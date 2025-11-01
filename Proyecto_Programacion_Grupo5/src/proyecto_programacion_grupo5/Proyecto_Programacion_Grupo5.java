59e3376986d31a925e2cadf8cb11c20d9c56ff20
Elí Mora Madrigal <elimm2005x@gmail.com>
2025-11-01 14:31:23 -0600

    Add files via upload

---
+++ Proyecto_Programacion_Grupo5/src/proyecto_programacion_grupo5/Proyecto_Programacion_Grupo5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_programacion_grupo5;
+
+/**
+ *
+ * @author elimm
+ */
+public class Proyecto_Programacion_Grupo5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
